1a9f01feb9dc66bb897eadaf865a226d64001b65
wangxinshui87 <wangxinshui87@users.noreply.github.com>
2015-04-28 18:59:54 +0800

    1.规范编程

--- src/main/java/com/hzmc/ema/CountRegistered.java
+++ src/main/java/com/hzmc/ema/CountRegistered.java
@@ -1,13 +1,17 @@
 package com.hzmc.ema;
 
-public class CountRegistered {
+public class CountRegistered
+{
 
-	public CountRegistered() {
+	public CountRegistered()
+	{
 		// TODO Auto-generated constructor stub
 	}
 
-	public void InitCount() {
-		while (true) {
+	public void InitCount()
+	{
+		while (true)
+		{
 			// 读取配置文件,构造注册用户消息
 			// 执行相关的sql语句
 		}
