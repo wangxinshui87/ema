4c438b48f48e1b51c999b1f3f28c6ced1a30c80f
wangxinshui87 <wangxinshui87@users.noreply.github.com>
2015-05-14 14:25:04 +0800

    不用的import去除

--- src/main/java/elastic/DataPercalator.java
+++ src/main/java/elastic/DataPercalator.java
@@ -1,19 +1,6 @@
 package elastic;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Timer;
-
-import org.codehaus.groovy.tools.groovydoc.Main;
-import org.elasticsearch.action.percolate.PercolateResponse;
-
-import elastic.bean.CpuObject;
-import elastic.bean.ElasticObject;
-import elastic.bean.ElasticQuery;
-import elastic.bean.Threshold;
-import elastic.manage.alarm.AlarmService;
-import elastic.manage.alarm.impl.AlarmServiceImpl;
-import elastic.manage.percolator.PercolateCpuObjectImpl;
 
 public class DataPercalator 
 {
